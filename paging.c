10ba67614260567831dc775ad86bb4b974b63879
STSonyThomas <75160254+STSonyThomas@users.noreply.github.com>
2023-01-08 19:43:04 +0530

    created a new paging memory technique program

--- paging.c
+++ paging.c
@@ -1,59 +1,101 @@
 #include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
 
-int main()
-{
- int ms, ps, nop, np, rempages, i, j, x, y, pa, offset;
- int s[10], fno[10][20];
-
-
-printf("\nEnter the memory size -- ");
-scanf("%d",&ms);
-
-printf("\nEnter the page size -- ");
-scanf("%d",&ps);
-
-nop = ms/ps;
-printf("\nThe no. of pages available in memory are -- %d ",nop);
-
-printf("\nEnter number of processes -- ");
- scanf("%d",&np);
-rempages = nop;
-for(i=1;i<=np;i++)
-
-{
-
-printf("\nEnter no. of pages required for p[%d]-- ",i);
- scanf("%d",&s[i]);
-
-if(s[i] >rempages)
-{
-
-printf("\nMemory is Full");
-break;
-}
-rempages = rempages - s[i];
-
-printf("\nEnter pagetable for p[%d] --- ",i);
- for(j=0;j<s[i];j++)
-scanf("%d",&fno[i][j]);
-}
-
-printf("\nEnter Logical Address to find Physical Address ");
-printf("\nEnter process no. and pagenumber and offset -- ");
-
-scanf("%d %d %d",&x,&y, &offset);
+//fucntion decalrations
+void createPage(int* remPage, int* noProcess,int* processPage);
+void calcPhysical(int pageSize,int maxpage,int* noProcess, int* processPage,int** pagingTable);
 
 
 
-if(x>np || y>=s[i] || offset>=ps)
-printf("\nInvalid Process or Page Number or offset");
 
-else
-{ pa=fno[x][y]*ps+offset;
-printf("\nThe Physical Address is -- %d",pa);
 
-}
-return 0;
-//getch();
+int main(){
+    int memSize,pageSize,noPage,noProcess,x,y,z,remPages;
+    int* processPage;
+    int** pagingTable;
+
+    printf("Enter the size of memory: ");
+    scanf("%d",&memSize);
+    printf("Enter pageSize : ");
+    scanf("%d",&pageSize);
+    // no of pages = memory size/ pageSize
+    noPage = memSize / pageSize;
+    printf("The no of pages = %d",noPage);
+
+    //preliminary preparation over
+
+    //asking user to write the number of processes
+
+    printf("Enter the number of processes: ");
+    scanf("%d",&noProcess);
+
+    //assigning in a dynamic fashion the no of rows for processPage(this contains the number of pages of each process)
+    processPage =(int*)malloc(sizeof(int)*noProcess);
+    //assigning size to 2 dimensional array for pagingTable
+    pagingTable =(int**)malloc(sizeof(int)*noProcess);
+    for(int i=0;i<noProcess;i++){
+        pagingTable[i]=(int*)malloc(sizeof(int)*noPage);
+        //because the maximum number of pages that a process can take
+        //is the max available pages in logical memory that is noPage
+    }
+
+
+    //setting the remaining unassigned pages to total number of pages initially
+    remPages = noPage;
+
+
+    createPage(&remPages,&noProcess,processPage);
+    calcPhysical(pageSize,noPage,&noProcess,processPage,pagingTable);
+
+    free(processPage);
+    free(pagingTable);
+    return 0;
+
 }
 
+
+
+
+
+
+//fucntion definitions
+
+void createPage(int* remPage, int* noProcess,int* processPage){
+    int pages;
+
+    for(int i=0;i<*noProcess;i++){
+        printf("Enter the no of Pages required for process%d : ",i+1);
+        scanf("%d",&pages);
+        if(pages>*remPage){
+            printf("Memory full,terminating now!");
+            break;
+        }
+    *remPage-=pages;
+    processPage[i]=pages;
+    }
+}
+
+//for pagingTable and physical memory calculation
+
+void calcPhysical(int pageSize,int maxpage,int* noProcess, int* processPage,int** pagingTable){
+    int pno,pgno,x;
+    for(int i=0;i<*noProcess;i++){
+        printf("Enter the paging of process %d\n",i+1);
+        for(int j=0;j<processPage[i];j++){
+            printf("for page %d\n",j+1);
+            scanf("%d",&pagingTable[i][j]);
+        }
+    }
+
+    printf("Enter the process no,page no,offset : ");
+    scanf("%d %d %d",&pno,&pgno,&x);
+
+    if(pno<=*noProcess &&x<=maxpage){
+        if(pgno<=processPage[pno-1]){
+            printf("\nthe physical address is: %d\n",(pagingTable[pno-1][pgno-1]*pageSize+x));
+        }
+    }else{
+        printf("Error in process no, page no or displacement\n");
+    }
+}
